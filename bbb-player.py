8367bc950dde287c545cbe459229d35b2c090251
timopetric <timopetric@hotmail.com>
2020-11-06 16:25:20 +0100

    add basic logging

--- bbb-player.py
+++ bbb-player.py
@@ -7,23 +7,30 @@ from distutils.dir_util import copy_tree
 import traceback
 import re
 from datetime import timedelta
+import logging
 
 
+LOGGING_LEVEL = logging.INFO
+# LOGGING_LEVEL = logging.DEBUG
 DOWNLOADED_FULLY_FILENAME = "rec_fully_downloaded.txt"
 DOWNLOADED_MEETINGS_FOLDER = "downloadedMeetings"
 DEFAULT_COMBINED_VIDEO_NAME = "combine-output"
 COMBINED_VIDEO_FORMAT = "mkv"
+
+logging.basicConfig(format="[%(asctime)s] [%(levelname)8s]: %(message)s",
+                    datefmt="%H:%M:%S",
+                    level=LOGGING_LEVEL)
+logger = logging.getLogger('bbb-player')
 
 
 def ffmpegCombine(suffix, fileName=DEFAULT_COMBINED_VIDEO_NAME):
     try:
         import ffmpeg
     except:
-        print("ffmpeg-python not imported. Try running:")
-        print("pip3 install ffmpeg-python")
+        logger.critical("ffmpeg-python not imported. Try running:\npip3 install ffmpeg-python")
         exit(1)
 
-    print("ffmpeg-python imported.")
+    logger.debug("ffmpeg-python imported")
 
     video_file = ffmpeg.input('./deskshare/deskshare.' + suffix)
     audio_file = ffmpeg.input('./video/webcams.' + suffix)
@@ -41,18 +48,18 @@ def downloadFiles(baseURL, basePath):
                   "favicon.ico"]
 
     for file in filesForDL:
-        print('Downloading ' + file)
+        logger.info('Downloading ' + file)
         downloadURL = baseURL + file
-        print(downloadURL)
+        logger.debug(downloadURL)
         savePath = os.path.join(basePath, file)
-        print(savePath)
+        logger.debug(savePath)
 
         try:
             urllib.request.urlretrieve(
                 downloadURL, savePath, reporthook=bar.on_urlretrieve if bar else None)
         except Exception:
-            traceback.print_exc()
-            print("Did not download " + file)
+            # traceback.print_exc()
+            logger.exception("Did not download " + file)
 
 
 def downloadSlides(baseURL, basePath):
@@ -60,9 +67,9 @@ def downloadSlides(baseURL, basePath):
     with open(basePath + '/presentation_text.json') as f:
         data = json.load(f)
         for element in data:
-            print(element)
+            logger.debug(element)
             noSlides = len(data[element])
-            print(noSlides)
+            logger.debug(noSlides)
             createFolder(os.path.join(basePath, 'presentation', element))
             for i in range(1, noSlides+1):
                 downloadURL = baseURL + 'presentation/' + \
@@ -70,14 +77,14 @@ def downloadSlides(baseURL, basePath):
                 savePath = os.path.join(basePath, 'presentation',
                                         element,  'slide-{}.png'.format(i))
 
-                print(downloadURL)
-                print(savePath)
+                logger.debug(downloadURL)
+                logger.debug(savePath)
 
                 try:
                     urllib.request.urlretrieve(
                         downloadURL, savePath, reporthook=bar.on_urlretrieve if bar else None)
                 except:
-                    print("Did not download " + element +
+                    logger.warning("Did not download " + element +
                           '/slide-' + str(i) + '.png')
 
 
@@ -86,9 +93,9 @@ def createFolder(path):
     try:
         os.makedirs(path)
     except OSError:
-        print("Creation of the directory %s failed" % path)
+        logger.debug("Creation of the directory %s failed" % path)
     else:
-        print("Successfully created the directory %s " % path)
+        logger.debug("Successfully created the directory %s " % path)
 
 
 # Parse the command line arguments
@@ -110,7 +117,7 @@ group.add_argument("-c", "--combine", type=str, nargs=1,
 args = parser.parse_args()
 
 if(args.download != None and args.play == args.combine == None):
-    print("Download")
+    logger.info("Download")
     inputURL = args.download[0]
 
     meetingNameWanted = None
@@ -124,28 +131,31 @@ if(args.download != None and args.play == args.combine == None):
     if matchesURL and len(matchesURL.groups()) == 2:
         bbbVersion = matchesURL.group(1)
         meetingId = matchesURL.group(2)
-        print("Detected bbb version:\t{}\nDetected meeting id:\t{}".format(bbbVersion, meetingId))
+        logger.info(f"Detected bbb version:\t{bbbVersion}")
+        logger.info(f"Detected meeting id:\t{meetingId}")
     else:
-        raise ValueError("Meeting ID could not be found in the url.")
+        logger.error("Meeting ID could not be found in the url.")
+        exit(1)
 
     baseURL = "{}://{}/presentation/{}/".format(urlparse(inputURL).scheme,
                                                 urlparse(inputURL).netloc,
                                                 meetingId)
-    print("Base url: {}".format(baseURL))
+    logger.debug("Base url: {}".format(baseURL))
 
     if meetingNameWanted:
         folderPath = os.path.join(os.getcwd(), DOWNLOADED_MEETINGS_FOLDER, meetingNameWanted)
     else:
         folderPath = os.path.join(os.getcwd(), DOWNLOADED_MEETINGS_FOLDER, meetingId)
-    print("Folder path: {}".format(folderPath))
+    logger.debug("Folder path: {}".format(folderPath))
 
     if os.path.isfile(os.path.join(folderPath, DOWNLOADED_FULLY_FILENAME)):
-        print("Meeting is already downloaded.")
+        logger.info("Meeting is already downloaded.")
     else:
-        print("Folder already created but not everything was downloaded. Retrying.")
+        logger.info("Folder already created but not everything was downloaded. Retrying.")
+        # todo: maybe delete contents of the folder
 
         foldersToCreate = [os.path.join(folderPath, x) for x in ["", "video", "deskshare", "presentation"]]
-        # print(foldersToCreate)
+        # logger.info(foldersToCreate)
         for i in foldersToCreate:
             createFolder(i)
 
@@ -154,8 +164,8 @@ if(args.download != None and args.play == args.combine == None):
             bar = ProgressBar(throttle=timedelta(seconds=1),
                 template="Download |{animation}|{tta}| {done:B}/{total:B} at {speed:B}/s")
         except:
-            print("progressist not imported. Progress bar will not be shown. Try running:")
-            print("pip3 install progressist")
+            logger.warning("progressist not imported. Progress bar will not be shown. Try running: \
+                            pip3 install progressist")
             bar = None
 
         downloadFiles(baseURL, folderPath)
@@ -169,28 +179,26 @@ if(args.download != None and args.play == args.combine == None):
             pass
 
 elif(args.play != None and args.name == args.download == args.combine == None):
-    print("Play")
+    logger.info("Play")
     fileId = args.play[0]
 
     try:
         from flask import Flask, redirect
     except:
-        print("Flask not imported. Try running:")
-        print("pip3 install Flask")
+        logger.error("Flask not imported. Try running:\npip3 install Flask")
         exit(1)
 
-    print("Flask imported.")
-
-    print(os.getcwd())
+    logger.debug("Flask imported.")
+    logger.debug(os.getcwd())
 
     try:
         os.chdir(os.path.join(os.getcwd(), 'downloadedMeetings', fileId))
     except:
-        print("Meeting with ID/name " + fileId +
-              " is not downloaded. Download it first using the --download command")
+        logger.error(f"Meeting with ID {fileId} is not downloaded. \
+                       Download it first using the --download command")
         exit(1)
 
-    print(os.getcwd())
+    logger.debug(os.getcwd())
 
     print('---------')
     print('In your modern web browser open:')
@@ -218,22 +226,22 @@ elif(args.play != None and args.name == args.download == args.combine == None):
         app.run()
 
 elif(args.combine != None and args.name == args.download == args.play == None):
-    print("Combine")
+    logger.info("Combine")
     fileIdOrName = args.combine[0]
 
     try:
         os.chdir('./downloadedMeetings/' + fileIdOrName)
     except:
-        print(f"Meeting with ID {fileIdOrName} \
-                is not downloaded. Download it first using the --download command")
+        logger.error(f"Meeting with ID {fileIdOrName} is not downloaded. \
+                       Download it first using the --download command")
         exit(1)
 
     matchesName = re.match(r"([0-9a-f]{40}-\d{13})", fileIdOrName, re.IGNORECASE)
     if matchesName:
         # if file id/name looks like bbb 54 char string use a simple predefined name
         meetingId = matchesName.group(0)
-        print(f"Extracted meeting id: {meetingId} from provided name")
-        print(f"Setting output file name to {DEFAULT_COMBINED_VIDEO_NAME}")
+        logger.info(f"Extracted meeting id: {meetingId} from provided name")
+        logger.info(f"Setting output file name to {DEFAULT_COMBINED_VIDEO_NAME}")
         fileName = DEFAULT_COMBINED_VIDEO_NAME
     else:
         fileName = fileIdOrName
@@ -241,19 +249,19 @@ elif(args.combine != None and args.name == args.download == args.play == None):
 
 
     if(os.path.isfile(f'./{fileName}.{COMBINED_VIDEO_FORMAT}')):
-        print(f'./{DEFAULT_COMBINED_VIDEO_NAME}.{COMBINED_VIDEO_FORMAT} already found. Aborting.')
+        logger.warning(f'./{DEFAULT_COMBINED_VIDEO_NAME}.{COMBINED_VIDEO_FORMAT} already found. Aborting.')
         exit(1)
     elif(os.path.isfile('./deskshare/deskshare.webm') and os.path.isfile('./video/webcams.webm')):
         ffmpegCombine('webm', fileName=fileName)
     elif(os.path.isfile('./deskshare/deskshare.mp4') and os.path.isfile('./video/webcams.mp4')):
         ffmpegCombine('mp4', fileName=fileName)
     else:
-        print('Video files not found, this meeting might not be supported.')
+        logger.error('Video files not found, this meeting might not be supported.')
         exit(1)
 
-    print('Your combined video file is located here:')
-    print(f'./downloadedMeetings/{fileIdOrName}/{fileName}.{COMBINED_VIDEO_FORMAT}')
+    logger.info('Your combined video file is located here:')
+    logger.info(f'./downloadedMeetings/{fileIdOrName}/{fileName}.{COMBINED_VIDEO_FORMAT}')
 
 else:
-    print("Error parsing aguments. Use '--help' for help.")
+    logger.error("Error parsing aguments. Use '--help' for help.")
 
